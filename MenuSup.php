e732f7485b223aff72c35f07ddfea5a552908c4b
KriegerLM <152136833+KriegerLM@users.noreply.github.com>
2023-11-27 01:37:49 -0600

    Add files via upload

---
+++ MenuSup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <link href="css3/MenuSup.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <header class="header">
+            <div class="contenedor logo-nav-contenedor">
+                <label><a href="#" class="logo">TechSchool</a></label>
+                <nav class="navegacion">
+                    <ul>
+                       <li><a href="index.php">Inicio</a></li>
+                       <li><a href="Catalogo.php">Catálogo</a></li>
+                       <li><a href="Asistencia.php">Asistencia</a></li>
+                       <li><a href="#">Datos</a></li>
+                       <li><a href="#">Usuario</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </body>
+</html>
